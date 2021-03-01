8eccf373957d0110705b7074b4db00a9a8533c3c
gormak00 <egor.makarchuk00@gmail.com>
2021-03-01 18:42:30 +0300

    split code to post and get methods, added getFilename to post methods

--- 1_lab/src/com/company/WebServer.java
+++ 1_lab/src/com/company/WebServer.java
@@ -1,9 +1,6 @@
 package com.company;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
 import java.text.DateFormat;
 import java.util.Date;
@@ -24,152 +21,202 @@ public class WebServer extends Thread {
     public void run() {
         try {
             InputStream is = s.getInputStream();
-            OutputStream os = s.getOutputStream();
 
             // буффер данных в 64 килобайта
             byte buf[] = new byte[64 * 1024];
             // читаем 64кб от клиента, результат - кол-во реально принятых данных
-            int r = is.read(buf);
+            int countOfData = is.read(buf);
 
             // создаём строку, содержащую полученую от клиента информацию
-            String request = new String(buf, 0, r + 1);
+            String request = new String(buf, 0, countOfData + 1);
+            if(request.startsWith("GET")){
+                getRequest(request, countOfData, buf);
+            } else if(request.startsWith("POST")){
+                postRequest(request, countOfData, buf);
+            } else if(request.startsWith("OPTIONS")){
 
-            // получаем путь до документа (см. ниже ф-ю "getPath")
-            String path = getPath(request);
-
-            // если из запроса не удалось выделить путь, то
-            // возвращаем "400 Bad Request"
-            if(path == null)
-            {
-                // первая строка ответа
-                String response = "HTTP/1.1 400 Bad Request\n";
-
-                // дата в GMT
-                DateFormat df = DateFormat.getTimeInstance();
-                df.setTimeZone(TimeZone.getTimeZone("GMT"));
-                response = response + "Date: " + df.format(new Date()) + "\n";
-
-                // остальные заголовки
-                response = response
-                        + "Connection: close\n"
-                        + "Server: SimpleWEBServer\n"
-                        + "Pragma: no-cache\n\n";
-
-                // выводим данные:
-                os.write(response.getBytes());
-
-                s.close();
-
-                return;
             }
-
-            // если файл существует и является директорией,
-            // то ищем файл HelloDY.html
-            File f = new File(path);
-            boolean flag = !f.exists();
-            if(!flag) if(f.isDirectory())
-            {
-                if(path.lastIndexOf(""+File.separator) == path.length()-1)
-                    path = path + "src/resources/HelloDY.html";
-                else
-                    path = path + File.separator + "src/resources/HelloDY.html";
-                f = new File(path);
-                flag = !f.exists();
-            }
-
-            // если по указанному пути файл не найден
-            // то выводим ошибку "404 Not Found"
-            if(flag)
-            {
-                // первая строка ответа
-                String response = "HTTP/1.1 404 Not Found\n";
-
-                // дата в GMT
-                DateFormat df = DateFormat.getTimeInstance();
-                df.setTimeZone(TimeZone.getTimeZone("GMT"));
-                response = response + "Date: " + df.format(new Date()) + "\n";
-
-                // остальные заголовки
-                response = response
-                        + "Content-Type: text/plain\n"
-                        + "Connection: close\n"
-                        + "Server: SimpleWEBServer\n"
-                        + "Pragma: no-cache\n\n";
-
-                // и гневное сообщение
-                response = response + "File " + path + " not found!";
-
-                // выводим данные:
-                os.write(response.getBytes());
-
-                s.close();
-
-                return;
-            }
-
-            // определяем MIME файла по расширению
-            // MIME по умолчанию - "text/plain"
-            String mime = "text/plain";
-
-            // выделяем у файла расширение (по точке)
-            r = path.lastIndexOf(".");
-            if(r > 0)
-            {
-                String ext = path.substring(r+1);
-                if(ext.equalsIgnoreCase("html"))
-                    mime = "text/html";
-                else if(ext.equalsIgnoreCase("htm"))
-                    mime = "text/html";
-                else if(ext.equalsIgnoreCase("png"))
-                    mime = "image/png";
-                else if(ext.equalsIgnoreCase("jpg"))
-                    mime = "image/jpeg";
-                else if(ext.equalsIgnoreCase("jpeg"))
-                    mime = "image/jpeg";
-                else if(ext.equalsIgnoreCase("bmp"))
-                    mime = "image/x-xbitmap";
-            }
-
-            // создаём ответ
-
-            // первая строка ответа
-            String response = "HTTP/1.1 200 OK\n";
-
-            // дата создания в GMT
-            DateFormat df = DateFormat.getTimeInstance();
-            df.setTimeZone(TimeZone.getTimeZone("GMT"));
-
-            // время последней модификации файла в GMT
-            response = response + "Last-Modified: " + df.format(new Date(f.lastModified())) + "\n";
-
-            // длина файла
-            response = response + "Content-Length: " + f.length() + "\n";
-
-            // строка с MIME кодировкой
-            response = response + "Content-Type: " + mime + "\n";
-
-            // остальные заголовки
-            response = response
-                    + "Connection: close\n"
-                    + "Server: SimpleWEBServer\n\n";
-
-            // выводим заголовок:
-            os.write(response.getBytes());
-
-            // и сам файл:
-            FileInputStream fis = new FileInputStream(path);
-            r = 1;
-            while(r > 0)
-            {
-                r = fis.read(buf);
-                if(r > 0) os.write(buf, 0, r);
-            }
-            fis.close();
 
             s.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private void postRequest(String request, int countOfData, byte buf[]) throws IOException {
+        OutputStream os = s.getOutputStream();
+
+        // получаем путь до документа (см. ниже ф-ю "getPath")
+        String path = getPath(request);
+
+        String filename = getFilenameFromRequest(request);
+
+        // если файл существует и является директорией,
+        // то ищем файл HelloDY.html
+        File f = new File(path);
+        boolean flag = !f.exists();
+        if(!flag) if(f.isDirectory())
+        {
+            if(path.lastIndexOf(""+File.separator) == path.length()-1)
+                path = path + "HelloDY.html";
+            else
+                path = path + File.separator + "HelloDY.html";
+            f = new File(path);
+            flag = !f.exists();
+        }
+    }
+
+    private String getFileBytesFromRequest(String request){
+        int index = request.indexOf("Content-Type:");
+        String otherPart = request.substring(index + 10);
+        int indexEnd = otherPart.indexOf("\"");
+        String filename = otherPart.substring(0, indexEnd);
+        return filename;
+    }
+
+    private String getFilenameFromRequest(String request){
+        int index = request.indexOf("filename");
+        String otherPart = request.substring(index + 10);
+        int indexEnd = otherPart.indexOf("\"");
+        String filename = otherPart.substring(0, indexEnd);
+        return filename;
+    }
+
+    private void getRequest(String request, int countOfData, byte buf[]) throws IOException {
+        OutputStream os = s.getOutputStream();
+
+        // получаем путь до документа (см. ниже ф-ю "getPath")
+        String path = getPath(request);
+
+        // если из запроса не удалось выделить путь, то
+        // возвращаем "400 Bad Request"
+        if(path == null)
+        {
+            // первая строка ответа
+            String response = "HTTP/1.1 400 Bad Request\n";
+
+            // дата в GMT
+            DateFormat df = DateFormat.getTimeInstance();
+            df.setTimeZone(TimeZone.getTimeZone("GMT"));
+            response = response + "Date: " + df.format(new Date()) + "\n";
+
+            // остальные заголовки
+            response = response
+                    + "Connection: close\n"
+                    + "Server: WebServer\n"
+                    + "Pragma: no-cache\n\n";
+
+            // выводим данные:
+            os.write(response.getBytes());
+
+            s.close();
+
+            return;
+        }
+
+        // если файл существует и является директорией,
+        // то ищем файл HelloDY.html
+        File f = new File(path);
+        boolean flag = !f.exists();
+        if(!flag) if(f.isDirectory())
+        {
+            if(path.lastIndexOf(""+File.separator) == path.length()-1)
+                path = path + "HelloDY.html";
+            else
+                path = path + File.separator + "HelloDY.html";
+            f = new File(path);
+            flag = !f.exists();
+        }
+
+        // если по указанному пути файл не найден
+        // то выводим ошибку "404 Not Found"
+        if(flag)
+        {
+            // первая строка ответа
+            String response = "HTTP/1.1 404 Not Found\n";
+
+            // дата в GMT
+            DateFormat df = DateFormat.getTimeInstance();
+            df.setTimeZone(TimeZone.getTimeZone("GMT"));
+            response = response + "Date: " + df.format(new Date()) + "\n";
+
+            // остальные заголовки
+            response = response
+                    + "Content-Type: text/plain\n"
+                    + "Connection: close\n"
+                    + "Server: WebServer\n"
+                    + "Pragma: no-cache\n\n";
+
+            // и гневное сообщение
+            response = response + "File " + path + " not found!";
+
+            // выводим данные:
+            os.write(response.getBytes());
+
+            s.close();
+
+            return;
+        }
+
+        // определяем MIME файла по расширению
+        // MIME по умолчанию - "text/plain"
+        String mime = "text/plain";
+
+        // выделяем у файла расширение (по точке)
+        countOfData = path.lastIndexOf(".");
+        if(countOfData > 0)
+        {
+            String ext = path.substring(countOfData+1);
+            if(ext.equalsIgnoreCase("html"))
+                mime = "text/html";
+            else if(ext.equalsIgnoreCase("htm"))
+                mime = "text/html";
+            else if(ext.equalsIgnoreCase("png"))
+                mime = "image/png";
+            else if(ext.equalsIgnoreCase("jpg"))
+                mime = "image/jpeg";
+            else if(ext.equalsIgnoreCase("jpeg"))
+                mime = "image/jpeg";
+            else if(ext.equalsIgnoreCase("bmp"))
+                mime = "image/x-xbitmap";
+        }
+
+        // создаём ответ
+
+        // первая строка ответа
+        String response = "HTTP/1.1 200 OK\n";
+
+        // дата создания в GMT
+        DateFormat df = DateFormat.getTimeInstance();
+        df.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+        // время последней модификации файла в GMT
+        response = response + "Last-Modified: " + df.format(new Date(f.lastModified())) + "\n";
+
+        // длина файла
+        response = response + "Content-Length: " + f.length() + "\n";
+
+        // строка с MIME кодировкой
+        response = response + "Content-Type: " + mime + "\n";
+
+        // остальные заголовки
+        response = response
+                + "Connection: close\n"
+                + "Server: WebServer\n\n";
+
+        // выводим заголовок:
+        os.write(response.getBytes());
+        System.out.println(response);
+
+        // и сам файл:
+        FileInputStream fis = new FileInputStream(path);
+        countOfData = 1;
+        while(countOfData > 0)
+        {
+            countOfData = fis.read(buf);
+            if(countOfData > 0) os.write(buf, 0, countOfData);
+        }
+        fis.close();
     }
 
     private String getPath(String header) {
